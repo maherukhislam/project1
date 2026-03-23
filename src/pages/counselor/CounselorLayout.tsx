aba27a7fa1562a30c45b2fec1b58fd49d36d77db
maherukh <maherukhislam2007@gmail.com>
2026-03-23 21:56:10 +0600

    I've fixed the CounselorLayout to match the working AdminLayout pattern

--- src/pages/counselor/CounselorLayout.tsx
+++ src/pages/counselor/CounselorLayout.tsx
@@ -47,7 +47,7 @@ const Sidebar: React.FC<SidebarProps> = ({ collapsed, setCollapsed, setMobileOpe
 
   return (
     <nav
-      className={`relative flex h-screen shrink-0 flex-col border-r border-emerald-950/10 bg-[#11251f] transition-all duration-300 ${
+      className={`relative flex h-full shrink-0 flex-col border-r border-emerald-950/10 bg-[#11251f] transition-all duration-300 ${
         collapsed ? 'w-16' : 'w-64'
       }`}
     >
@@ -184,9 +184,9 @@ const CounselorLayout: React.FC = () => {
   return (
     <div className="flex min-h-screen w-full bg-[#0b1512] text-white">
       {/* Desktop sidebar */}
-      <aside className="sticky top-0 hidden h-screen shrink-0 lg:block">
+      <div className="sticky top-0 hidden h-screen lg:flex">
         <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} setMobileOpen={setMobileOpen} />
-      </aside>
+      </div>
 
       <div
         className={`fixed inset-y-0 left-0 z-50 flex h-full transition-transform duration-300 lg:hidden ${
